17c4854698e2e6fc00cde06d6f2d5cf3953e8a38
Ayoub Abdenour <aabdenou@c3r10p1.1337.ma>
2024-07-03 20:05:55 +0100

    expand 96%

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: aabdenou <aabdenou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 21:50:49 by aabdenou          #+#    #+#             */
-/*   Updated: 2024/07/03 16:11:54 by aabdenou         ###   ########.fr       */
+/*   Updated: 2024/07/03 20:04:31 by aabdenou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,13 @@ void expand(t_lexer *lexer, t_env *env)
     {
         int i = 0;
         char *str_to_expand = NULL;
+        // if (lexer->tokens == HEREDOC)
+        // {
+        //     if (lexer->next && lexer->tokens == WHITESPACE)
+        //         lexer = lexer->next; 
+        //     lexer = lexer->next;
+        //     continue;
+        // }
         if(ft_strcmp(lexer->str,"$") == 0)
             lexer = lexer->next;
         if(!lexer)
