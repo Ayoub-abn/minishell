0a78dd11b8619d641f429e88d79bbb18d8c76e56
Ayoub Abdenour <aabdenou@c3r10p4.1337.ma>
2024-07-03 16:03:52 +0100

    expand 90%

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: aabdenou <aabdenou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 21:50:49 by aabdenou          #+#    #+#             */
-/*   Updated: 2024/07/03 00:51:57 by aabdenou         ###   ########.fr       */
+/*   Updated: 2024/07/03 15:57:43 by aabdenou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,27 @@ char *to_expand(char *str) {
 // Function to expand variables in lexer tokens
 void expand(t_lexer *lexer, t_env *env) 
 {
-    // if(ft_strcmp(lexer->str,"$") == 0)
-    // {
-    //     printf("i\n");
-    //     // lexer = lexer->next;
-    // }
     while (lexer) 
     {
-        if (lexer->tokens == WORD && lexer->str[0] != '\'') {
-            int i = 0;
-            char *str_to_expand = NULL;
+        int i = 0;
+        char *str_to_expand = NULL;
+        if(ft_strcmp(lexer->str,"$") == 0)
+            lexer = lexer->next;
+        if(!lexer)
+            break;
+        if (lexer->tokens == WORD && lexer->str[0] != '\'') 
+        {
             while (lexer->str[i]) 
             {
+                if(lexer->str[i] == '$' && lexer->str[i+1] == '$')//echo $$
+                    str_to_expand = ft_strjoin(str_to_expand, "$");
                 if (lexer->str[i] == '$') 
                 {
                     char *key = to_expand(&lexer->str[i]);
                     char *value = get_env_value(env, key);
                     i += ft_strlen(key) + 1;
-                    if (value) 
-                    {
+                    if (value)
                         str_to_expand = ft_strjoin(str_to_expand, value);
-                    }
                     free(key);
                 }
                  else
